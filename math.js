3d2712f1fc760f275ac8ba27123063bdd5a5cfea
Erik Raser <erikraser@Eriks-MacBook-Pro-2.local>
2016-02-29 08:52:48 -0500

    added tests/code for =-*/

--- math.js
+++ math.js
@@ -1,2 +1,19 @@
 // export a function for doing addition
-exports.add = function 
+exports.add = function(a, b) {
+	return a + b;
+};
+
+// export a function for doing subtraction
+exports.subtract = function(a, b) {
+	return a - b;
+};
+
+// export a function for doing mulitplication
+exports.multiply = function(a, b) {
+	return a * b;
+};
+
+// export a function for doing division
+exports.divide = function(a, b) {
+	return a / b;
+};
